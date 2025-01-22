62cc040f61b929c80c53cc1ae55b2cacf4b1678d
KaitouJap <kaitoujap@gmail.com>
2025-01-22 10:13:09 +0100

    Added match model to prisma and the seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, team } from "@prisma/client";
 import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
@@ -16,22 +16,38 @@ async function main() {
         "Athletic Bilbao", 
         "Celta Vigo"
     ];
+    const teamResult: team[] = [];
 
-   for (const team of teams){
-    await prisma.team.create({
-        data: {
-            name: team
-        }
-    });
-   }
+    for (const team of teams){
+        const res = await prisma.team.create({
+            data: {
+                name: team
+            }
+        });
+        teamResult.push(res);
+    }
 
+    let min = teamResult[0].id;
+    let max = teamResult[teamResult.length - 1].id;
     for (let i = 0; i < 100; i++) {
         await prisma.player.create({
             data: {
                 name: faker.person.fullName(),
                 goalCount: faker.number.int({ min: 0, max: 100 }),
                 birthDate: faker.date.birthdate(),
-                teamId: faker.number.int({ min: 1, max: 10 })
+                teamId: faker.number.int({ min: min, max: max })
+            }
+        });
+    }
+
+    for (let i = 0; i < 25; i++) {
+        await prisma.match.create({
+            data: {
+                date: faker.date.recent(),
+                homeTeamId: faker.number.int({ min: min, max: max }),
+                awayTeamId: faker.number.int({ min: min, max: max }),
+                homeGoals: faker.number.int({ min: 0, max: 5 }),
+                awayGoals: faker.number.int({ min: 0, max: 5 })
             }
         });
     }
